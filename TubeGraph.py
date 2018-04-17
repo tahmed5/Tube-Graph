9bd5a8067e926341f950f7fa3ed3a28bb4cb54e1
tahmed5 <31918795+tahmed5@users.noreply.github.com>
2018-04-17 09:30:47 +0100

    Add files via upload

--- TubeGraph.py
+++ TubeGraph.py
@@ -1,5 +1,5 @@
 import random
-
+#1: [52, 73, 73, 234, 265]
 def station_data():
     global stations
     stations = {} #holds station key with station name 
@@ -9,32 +9,63 @@ def station_data():
         items = line.split(',')
         stations[int(items[0])] = items[3]
 
+
 def connections_data():
     global connections
     global connected_lines
+    global text_connections
     connections = {} #holds station 1 key and station 2 key
     connected_lines = {} #holds concatenated key of both stations and lines
+
+    text_connections = {}
     
     file = open('connections.csv','r')
     lines = file.read().splitlines()
     for line in lines:
         items = line.split(',')
         both_stations = items[0] + items[1]
-        if both_stations not in connected_lines.keys():
-             connected_lines[both_stations] = []
+        flipped_both_stations = items[1] + items[0]
 
-        connected_lines[both_stations].append(items[2])
+        if (both_stations not in connected_lines.keys()) and (flipped_both_stations not in connected_lines.keys() ):
+            connected_lines[both_stations] = []
+            
+        if both_stations in connected_lines.keys():
+            connected_lines[both_stations].append(items[2])
+
+        if flipped_both_stations in connected_lines.keys():
+            connected_lines[flipped_both_stations].append(items[2])
+
+            
         
         station1 = int(items[0])
         station2 = int(items[1])
+
+        text_station1 = stations.get(station1)
+        text_station2 = stations.get(station2)
+
+        ########
+
+        if text_station1 not in text_connections.keys():
+            text_connections[text_station1] = []
+
+        if text_station2 not in text_connections.keys():
+            text_connections[text_station2] = []
+
+        text_connections[text_station2].append(text_station1)
+        text_connections[text_station1].append(text_station2)           
+
+
+        #######
         
         if station1 not in connections.keys():
             connections[station1] = []
         if station2 not in connections.keys():
             connections[station2] = []
-            
-        connections[station1].append(station2)
+
         connections[station2].append(station1)
+        connections[station1].append(station2)
+
+
 
 def lines_data():
     global tube_lines
@@ -48,23 +79,32 @@ def lines_data():
         tube_lines[int(items[0])] = items[1]
 
 
+
 def station_lines(current, neighbour):
-    temp_station = {}
     current,neighbour = str(current), str(neighbour)
     connectedkey = current + neighbour
+
     
     if connected_lines.get(connectedkey) == None:
         connectedkey = neighbour + current
+        
+    if connectedkey not in temp_station.keys():
+        temp_station[connectedkey] = []
+        for line in connected_lines[connectedkey]:
+            temp_station[connectedkey].append(line)
 
-    for line in connected_lines[connectedkey]:
-        temp_station[connectedkey] = line
-
-    print(temp_station)
+    line = tube_lines[int(temp_station[connectedkey][0])]
     
-    print(current,neighbour)
+    del temp_station[connectedkey][0]
+
+    return line
+
+
+
 
     
 def free_roam():
+    print(text_connections)
     print('Which station would you like to start from ')
     print('If you want to quit enter "Q"')
     print('============================================')
@@ -86,13 +126,16 @@ def free_roam():
 
     roaming = True
     current_station = start_station_key
-    station_hold = start_station_key
+    first_station = start_station_key
+    line = None
     while roaming == True:
         print('You are at:', stations[current_station])
         print('Do you want to go to:')
+        global temp_station
+        temp_station = {}
         for x in range(len(connections[current_station])):
-            print(x + 1, stations[connections[current_station][x]], 'via' , station_lines(current_station, connections[current_station][x]) )
-            
+            print(x + 1, stations[connections[current_station][x]], 'via' , station_lines(current_station, connections[current_station][x]))
+        print(temp_station)
         user_station = int(input('Enter The Number Of The Station You Wish To Visit\n'))
         user_station -= 1
         if 0 <= user_station <= len(connections[current_station]) - 1:
